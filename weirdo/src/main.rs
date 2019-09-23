21cbb0767f90beb854c61c17ff1ea75ed508d101
Artur Ciesielski <artur.ciesielski@gmail.com>
2019-09-23 18:04:55 +0200

    Add config parser with TOML

--- weirdo/src/main.rs
+++ weirdo/src/main.rs
@@ -1,3 +1,29 @@
+extern crate clap;
+extern crate serde;
+extern crate toml;
+
+mod config;
+
+use config::parse_cfg_file;
+use clap::{App, Arg};
+
+
 fn main() {
-    println!("Hello, world from weirdo!");
+    let params = get_arg_parser().get_matches();
+    let config = parse_cfg_file(params.value_of("config").unwrap()).unwrap();
+
+    println!("{}", config.state.pidfile);
+}
+
+fn get_arg_parser<'a, 'b>() -> clap::App<'a, 'b> {
+    App::new("weirdo")
+        .version("0.1.0")
+        .author("Artur Ciesielski <artur.ciesielski@gmail.com>")
+        .about("Fast, lightweight and reliable PostgreSQL master-to-master replication tool.")
+        .arg(Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("cfg_file")
+                .help("Override default config file location")
+                .default_value("/etc/weidro.cfg"))
 }
